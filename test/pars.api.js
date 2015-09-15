0b7d18b4cc59ebdb5649f5002eba6398ed7cf659
magnus <Magnusbackyard@live.com>
2015-09-15 12:01:37 +0800

    pars constructor & test cases

---
+++ test/pars.api.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var should  = require("should");
+
+var Pars = require('../lib/pars');
+
+describe('Access Pars.js APIs', () => {
+    it ('should instantiate when given correct language', () => {
+        let pars = new Pars({
+            lang: 'zh-cn'
+        });
+        should(pars).be.an.instanceof(Pars);
+    });
+
+    it ('should throw exception when not given language', () => {
+        should(() => { new Pars(undefined); }).throw(TypeError);
+    });
+
+    it ('should throw exception when given unsupported language', () => {
+        should(() => { new Pars({ lang: 'fr' } ); }).throw(Error);
+    });
+
+    it ('should throw exception when given invalid lang property', () => {
+        should(() => { new Pars({ lang: 5 } ); }).throw(TypeError);
+    });
+});
