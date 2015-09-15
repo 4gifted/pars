1f800771ad5079e3ebd136a6f2c0e3b5c2d872bc
magnus <Magnusbackyard@live.com>
2015-09-15 19:26:42 +0800

    pars api & test cases

--- test/pars.api.js
+++ test/pars.api.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var should  = require("should");
+var should  = require("should"),
+    sinon = require('sinon');
 
 var Pars = require('../lib/pars');
 
@@ -26,5 +27,57 @@ describe('Access Pars.js APIs', () => {
 
     it ('should throw exception when given invalid lang property', () => {
         should(() => { new Pars({ lang: 5 } ); }).throw(TypeError);
+    });
+
+    it('should init plugin function property when given valid plugin', () => {
+        function pluginFn () {}
+        let pars = new Pars({
+            lang: 'zh-cn',
+            plugins: {
+                fn: pluginFn
+            }
+        });
+
+        should(pars.fn).be.an.Function;
+    });
+
+    it('should no such function property when given invalid plugin', () => {
+        let pars = new Pars({
+            lang: 'zh-cn',
+            plugins: {
+                fn: 'any string'
+            }
+        });
+
+        should(pars.fn).not.be.an.Function;
+    });
+
+    it('should invoke the plugin when plugin initialized successfully', () => {
+        let pluginSpy = sinon.spy();
+
+        let pars = new Pars({
+            lang: 'zh-cn',
+            plugins: {
+                fn: pluginSpy
+            }
+        });
+
+        pars.fn('process text');
+        should(pluginSpy.calledOnce).be.true;
+    });
+
+    it('should pass the extra arguments to plugin', () => {
+        let pluginSpy = sinon.spy();
+
+        let pars = new Pars({
+            lang: 'zh-cn',
+            plugins: {
+                fn: pluginSpy
+            }
+        });
+
+        pars.fn('process text', 'extra1', 50, {arg: 1.0, arg1: ['a', 'b']});
+        should(pluginSpy.calledOnce).be.true;
+        should(pluginSpy.calledWith(sinon.match.object, 'extra1', 50, {arg: 1.0, arg1: ['a', 'b']})).be.true;
     });
 });
